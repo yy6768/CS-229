08e1dbea6dfce149ae337eecfcb7c68997db48e5
yyorange <1258720926@qq.com>
2023-03-18 23:49:04 +0800

    init

---
+++ 作业/ps0/PS0.md
@@ -0,0 +1,31 @@
+# PS0
+
+## q1
+
+![70f109f04b1ba04d2d7663c8f219ec0](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/70f109f04b1ba04d2d7663c8f219ec0.jpg)
+
+![33e4f17f6c2b3a756ec4df66f420253](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/33e4f17f6c2b3a756ec4df66f420253.jpg)
+
+![5e6e5e9a352204e0472532ca025b797](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/5e6e5e9a352204e0472532ca025b797.jpg)
+
+![b9a25304fb12711755cd75012a25665](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/b9a25304fb12711755cd75012a25665.jpg)
+
+
+
+## q2
+
+![b42cf70f606f4829c258cac8fe65e61](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/b42cf70f606f4829c258cac8fe65e61.jpg)
+
+![d205ac84f30ea24dcdd7269fc245fd3](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/d205ac84f30ea24dcdd7269fc245fd3.jpg)
+
+![21d866fe1086a644a635be653d68a62](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/21d866fe1086a644a635be653d68a62.jpg)
+
+
+
+## q3
+
+![a50a2720aa53eed3c219c6d5bb579b0](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/a50a2720aa53eed3c219c6d5bb579b0.jpg)
+
+![c459202edb5dec82a85b6f486d1e639](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/c459202edb5dec82a85b6f486d1e639.jpg)
+
+![450cbdff9becfa25f1692349bf92758](http://typora-yy.oss-cn-hangzhou.aliyuncs.com/img/450cbdff9becfa25f1692349bf92758.jpg)
---
+++ 作业/ps1/src/p01b_logreg.py
@@ -0,0 +1,105 @@
+import numpy as np
+import util
+
+from linear_model import LinearModel
+
+
+def main(train_path, eval_path, pred_path):
+    """Problem 1(b): Logistic regression with Newton's Method.
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        eval_path: Path to CSV file containing dataset for evaluation.
+        pred_path: Path to save predictions.
+    """
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+
+    # *** START CODE HERE ***
+    logistic_regression = LogisticRegression()
+    logistic_regression.fit(x_train, y_train)
+
+    x_eval, y_eval = util.load_dataset(eval_path)
+    y_hat = logistic_regression.predict(x_eval)
+    util.plot(x_train, y_train, logistic_regression.theta, 'output/p01b_{}.png'.format(pred_path[-5]))
+    np.savetxt(pred_path, np.array(y_hat), fmt="%d")
+    # *** END CODE HERE ***
+
+
+def logistic(z):
+    return 1 / (1 + np.exp(-z))
+
+
+class LogisticRegression(LinearModel):
+    """Logistic regression with Newton's Method as the solver.
+
+    Example usage:
+        > clf = LogisticRegression()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def __init__(self, step_size=0.2, max_iter=100, eps=1e-5, theta_0=None, verbose=True):
+        super().__init__(step_size, max_iter, eps, theta_0, verbose)
+
+    def logistic_hessian(self, x):
+        m, _ = x.shape
+        h_theta = logistic(x @ self.theta)
+        hessian = (x.T * h_theta * (1 - h_theta)).dot(x) / m
+        return hessian
+
+    def grad(self, x, y):
+        m, n = x.shape
+        gradient = np.zeros(n)
+        for i in range(m):
+            gradient += (y[i] - logistic(np.dot(self.theta, x[i]))) * x[i]
+        return - 1 / m * gradient
+
+    def fit(self, x, y):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
+
+        Args:
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
+        """
+        # *** START CODE HERE ***
+        # init
+        m, n = x.shape
+        x0 = np.ones(m)
+        np.insert(x, n, x0, axis=1)
+
+        if self.theta is None:
+            self.theta = np.zeros(n)
+
+        # iteration
+        k = 0
+
+        while k < self.max_iter:
+            hessian = self.logistic_hessian(x)
+            ini_hessian = np.linalg.inv(hessian)
+            gradient = self.grad(x, y)
+            delta = ini_hessian @ gradient
+            self.theta -= delta
+            # print(f'theta:\n {self.theta}')
+            k += 1
+            if np.linalg.norm(delta, ord=1) < self.eps:
+                break
+
+    # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (m, n).
+
+        Returns:
+            Outputs of shape (m,).
+        """
+        # *** START CODE HERE ***
+        m, n = x.shape
+        x0 = np.ones(m)
+        x = np.insert(x, n, x0, axis=1)
+        h_theta = logistic(x @ self.theta)
+        y_hat = [0 if x > 0.5 else 1 for x in h_theta]
+        return y_hat
+        # *** END CODE HERE ***
